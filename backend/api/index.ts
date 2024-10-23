5d216d007d2c0953f2f43efd5f291eb12d79dfe8
Long Tran <hoanglongtran2002@gmail.com>
2024-10-23 18:54:02 +1100

    delete comment out line

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -6,7 +6,6 @@ import errorHandler from 'middleware-http-errors';
 import config from './config.json';
 
 const app = express();
-// const PORT = process.env.PORT || port;
 const PORT: number = parseInt(process.env.PORT || config.port);
 const HOST: string = process.env.IP || 'localhost';
 
